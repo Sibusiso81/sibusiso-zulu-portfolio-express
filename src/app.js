19d8d42b654cb673d98205e003c2d6aa7a98967b
Sibusiso81 <zulusibusiso81@gmail.com>
2023-12-24 14:02:22 +0200

    updating

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
-const express= require('express');/* getting access to the express framework */
+const express= require('express'); /* getting access to the express framework */
 const path = require('path');/* A utility to construct full system paths based on relative paths  */
 const app = express();/* creating an express app */
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
+/* configure mustache */
 app.use(express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,'./pages'));
+app.set('view engine','mustache');
+app.engine('mustache',mustacheExpress());
 
 /* accessing server root level route *res-request ,res(response object)*/
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'pages/index.html'));
+   /*  res.sendFile(path.join(__dirname,'pages/index.html')); */
+    res.render()    
 });
 
 /* Run the server */
---
+++ src/jobs.js
@@ -0,0 +1,39 @@
+const JOBS = [
+  {
+    id: 1,
+    title: "Frontend Dveloper",
+    location: "Johannesburg,South Africa",
+    salary: "R45 000 p/m",
+    posted: "Mar 3,2023",
+  },
+  {
+    id: 2,
+    title: "Fullstack Developer",
+    locatio: "Johannesburg,South Africa",
+    salary: "R57 000 p/m",
+    posted: "Jan 17,2023",
+  },
+  {
+    id: 3,
+    title: "Data Scientis",
+    location: "Cape Towm,South Africa",
+    salary: "R64 000 p/m ",
+    posted: "Mar 6 ,2023",
+  },
+  {
+    id: 4,
+    title: "ML Engineer",
+    location: "Durban,South Africa",
+    salary: "R 80 000 p/m",
+    posted: "5 Apr ,2023",
+  },
+  {
+    id: 5,
+    title: "Backend Developer",
+    location: "Cape Town,South Afirca",
+    salary: "R47 00 p/m",
+    posted: "16 Feb ,2023",
+  },
+];
+
+module.exports = JOBS;
